f94617208d777bd2b8729c17ea5f4e79fc3cbde2
Riley Walsh <rwalsh@nationalstorage.com.au>
2025-01-09 10:10:46 +1000

    init datingapp commit

---
+++ API/Entities/AppUser.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace API.Entities;
+
+public class AppUser
+{
+    public int Id { get; set; }   //get set is like read write
+    public required string UserName { get; set; } //use required as each user will need a name
+}
