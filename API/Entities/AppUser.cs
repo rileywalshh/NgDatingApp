33ac5a7af94aee9126025d67ae8e46f9024eca96
rileywalshh <rileywalshh@gmail.com>
2025-01-09 13:23:10 +1000

    modified api

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -6,4 +6,6 @@ public class AppUser
 {
     public int Id { get; set; }   //get set is like read write
     public required string UserName { get; set; } //use required as each user will need a name
+    public required byte[] PasswordHash {get; set;}
+    public required byte[] PasswordSalt {get; set;}
 }
