f0c1d444db7bdd0b318640017e397d2d0f1a19c3
rileywalshh <rileywalshh@gmail.com>
2025-01-13 12:40:56 +1000

    added register method

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9524a28ebe940c17d8a81b23974a77ed68ad61bf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce3bdc3deb723e64f7d207fcc758c3621b6a0979")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
