82e19364927b04e48033b65b2b2d693395899c71
rileywalshh <rileywalshh@gmail.com>
2025-01-13 14:01:04 +1000

    added new theme and auth

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce3bdc3deb723e64f7d207fcc758c3621b6a0979")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f0c1d444db7bdd0b318640017e397d2d0f1a19c3")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
