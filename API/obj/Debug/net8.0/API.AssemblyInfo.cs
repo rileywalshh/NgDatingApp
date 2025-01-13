ce3bdc3deb723e64f7d207fcc758c3621b6a0979
rileywalshh <rileywalshh@gmail.com>
2025-01-13 10:59:39 +1000

    added conditionals for login logout

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8686434ae8368be25c7c02adeb7db38b5a2a4507")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9524a28ebe940c17d8a81b23974a77ed68ad61bf")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
