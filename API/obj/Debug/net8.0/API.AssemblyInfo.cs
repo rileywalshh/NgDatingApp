853ebccfe94d6148c9c84f3e6d5e4435f787276f
rileywalshh <rileywalshh@gmail.com>
2025-01-14 12:48:55 +1000

    added error validation

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+95f297dd6916588eccc99640f71ec23c04c947f4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+82227dfdfb67ee4fdbf0bc589d475513e049eae6")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
